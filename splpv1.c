94dfa020daacc56c0def95cf3c49b597d035d447
evgexabu@gmail.com <evgexabu@gmail.com>
2021-11-04 23:29:41 +0300

    local changes

--- splpv1.c
+++ splpv1.c
@@ -112,47 +112,34 @@ const char base64[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                         0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 };
 
+
+const char* commands[] = { "GET_DATA", "GET_FILE", "GET_COMMAND" };
+
 enum cur_status current_status = INIT;
-char* pre_CMD;
+int pre_CMD = -1;
 
 enum test_status validate_message( struct Message *msg )
 {
     
     // TODO: Implement me
 
-    enum Direction	direction = msg->direction;
     char* text_message; 
     text_message = msg->text_message;
-    strcpy(text_message, msg->text_message);
-    if (current_status != WAITING_DATA) {
-        pre_CMD = "";
-    }
-    if (direction == A_TO_B) {
-        if (current_status == INIT) {
-            if (!strcmp(text_message, "CONNECT")) {
-                current_status = CONNECTING;
-                pre_CMD = "";
-                return MESSAGE_VALID;
-            }
-        }
-        else if (current_status == CONNECTED) {
-            if (!strcmp(text_message, "GET_VER")) {
-                current_status = WAITING_VER;
+    if (msg->direction == A_TO_B) {
+       if (current_status == CONNECTED) {
+            if (!strcmp(text_message, "GET_FILE")) {
+                current_status = WAITING_DATA;
+                pre_CMD = 1;
                 return MESSAGE_VALID;
             }
             else if (!strcmp(text_message, "GET_DATA")) {
                 current_status = WAITING_DATA;
-                pre_CMD = "GET_DATA";
-                return MESSAGE_VALID;
-            }
-            else if (!strcmp(text_message, "GET_FILE")) {
-                current_status = WAITING_DATA;
-                pre_CMD = "GET_FILE";
+                pre_CMD = 0;
                 return MESSAGE_VALID;
             }
             else if (!strcmp(text_message, "GET_COMMAND")) {
                 current_status = WAITING_DATA;
-                pre_CMD = "GET_COMMAND";
+                pre_CMD = 2;
                 return MESSAGE_VALID;
             }
             else if (!strcmp(text_message, "GET_B64")) {
@@ -163,6 +150,48 @@ enum test_status validate_message( struct Message *msg )
                 current_status = DISCONNECTING;
                 return MESSAGE_VALID;
             }
+            else if (!strcmp(text_message, "GET_VER")) {
+                current_status = WAITING_VER;
+                return MESSAGE_VALID;
+            }            
+       }
+       else if (current_status == INIT) {
+           if (!strcmp(text_message, "CONNECT")) {
+               current_status = CONNECTING;
+               return MESSAGE_VALID;
+           }
+       }
+    }
+    else if (current_status == WAITING_DATA && pre_CMD != -1) {
+        int sizeCMD = strlen(commands[pre_CMD]);
+        if (!strncmp(text_message, commands[pre_CMD], sizeCMD)) {
+            int sizeMSG = strlen(text_message);
+            char* temp = text_message + sizeMSG;
+            if (!strncmp(temp - sizeCMD, commands[pre_CMD], sizeCMD)) {
+                if (!strncmp(text_message + sizeCMD, " ", 1)) {
+                    if (!strncmp(temp - sizeCMD - 1, " ", 1)) {
+                        text_message += sizeCMD + 1;
+                        for (; data[*text_message]; ++text_message);
+                        if (strlen(text_message + sizeCMD +1) == 0) {
+                            current_status = CONNECTED;
+                            return MESSAGE_VALID;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    else if (current_status == WAITING_B64_DATA) {
+        if (!strncmp(text_message, "B64: ", 5)) {
+            text_message += 5;
+            char* temp = text_message;
+            for (; base64[*text_message]; ++text_message);
+            char equality = 0;
+            for (; (equality < 2) && (text_message[equality] == '='); ++equality);
+            if ((text_message - temp + equality) % 4 == 0 && !text_message[equality]) {
+                current_status = CONNECTED;
+                return MESSAGE_VALID;
+            }
         }
     }
     else if (current_status == CONNECTING) {
@@ -171,84 +200,138 @@ enum test_status validate_message( struct Message *msg )
             return MESSAGE_VALID;
         }
     }
+    else if (current_status == DISCONNECTING) {
+        if (!strcmp(text_message, "DISCONNECT_OK")) {
+            current_status = INIT;
+            return MESSAGE_VALID;
+        }
+    }
     else if (current_status == WAITING_VER) {
         if (!strncmp(text_message, "VERSION ", 8)) {
             text_message += 8;
             if (*text_message > 48 && *text_message < 58) {
                 text_message++;
-                for (; *text_message != '\0'; text_message++)
+                for (; *text_message != '\0'; text_message ++)
+                    /*text_message += 8;
+                    int l = strlen(text_message);
+                    for (int i = 0; i < l; ++i) {
+                        if (!data[*(text_message + i)]) {
+                            current_status = INIT;
+                            return MESSAGE_INVALID;
+                        }
+                    }*/
                     if (*text_message < 48 || *text_message > 57) {
                         current_status = INIT;
                         return MESSAGE_INVALID;
                     }
                 current_status = CONNECTED;
                 return MESSAGE_VALID;
-            }            
-        }
-    }
-    else if (current_status == WAITING_DATA && (strcmp(pre_CMD, "") != 0)) {
-        int sizeCMD = strlen(pre_CMD);
-        int sizeMSG = strlen(text_message);
-        if ( !strncmp(text_message, pre_CMD, sizeCMD) &&
-             !strncmp(text_message + sizeMSG - sizeCMD, pre_CMD, sizeCMD) &&
-             !strncmp(text_message + sizeCMD, " ", 1) &&
-             !strncmp(text_message + sizeMSG - sizeCMD - 1, " ", 1) ) {
-            text_message += sizeCMD + 1;
-            int l = strlen(text_message) - sizeCMD * 2 - 2;
-            for (int i = 0; i < l; ++i) {
-                if (!data[*(text_message + i)]) {
-                    current_status = INIT;
-                    return MESSAGE_INVALID;
-                }
-                /*if ( !((*(text_message + i) > 96 && *(text_message + i) < 123)) && 
-                     !((*(text_message + i) > 47 && *(text_message + i) < 58))  &&
-                     (*(text_message + i) != 46) ){
-                    current_status = INIT;
-                    return MESSAGE_INVALID;
-                }*/
             }
-            current_status = CONNECTED;
-            return MESSAGE_VALID;         
         }
     }
-    else if (current_status ==  WAITING_B64_DATA) {
-        if (!strncmp(text_message, "B64: ", 5)) {
-            text_message += 5;
-            int sizeMSG = strlen(text_message);
-            if (sizeMSG % 4 == 0) {
-                if (!strncmp(text_message + sizeMSG - 2, "=", 1) ) {
-                    if (!strncmp(text_message + sizeMSG - 1, "=", 1)) {
-                        sizeMSG -= 2;
-                    }
-                }
-                else if ( !strncmp(text_message + sizeMSG - 1, "=", 1) ) {
-                    sizeMSG--;
-                }
-                for (int i = 0; i < sizeMSG; ++i) {
-                    if (!base64[*(text_message + i)]) {
-                        current_status = INIT;
-                        return MESSAGE_INVALID;
-                    }
-                    /*if (!((*(text_message + i) > 96 && *(text_message + i) < 123)) &&
-                        !((*(text_message + i) > 64 && *(text_message + i) < 91)) &&
-                        !((*(text_message + i) > 47 && *(text_message + i) < 58)) &&
-                        (*(text_message + i) != 43) &&
-                        (*(text_message + i) != 47)) {
-                        current_status = INIT;
-                        return MESSAGE_INVALID;
-                    }*/
-                }                
-                current_status = CONNECTED;
-                return MESSAGE_VALID;
-            }            
-        }
-    }
-    else if (current_status == DISCONNECTING) {
-        if (!strcmp(text_message, "DISCONNECT_OK")) {
-            current_status = INIT;
-            return MESSAGE_VALID;
-        }
-    } 
     current_status = INIT;
     return MESSAGE_INVALID;
 }
+
+//
+//int CurrentState = 1;
+//int WhatCommand;
+//
+//const char base64[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 };
+//
+//const char data[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 };
+//
+//const char* commands[] = { "GET_DATA", "GET_FILE", "GET_COMMAND" };
+//
+//
+//enum test_status validate_message(struct Message* msg) {
+//    char* message = msg->text_message;
+//    if (msg->direction == A_TO_B) {
+//        if (CurrentState == 1 && !strcmp(message, "CONNECT")) {
+//            CurrentState = 2;
+//            return MESSAGE_VALID;
+//        }
+//        else if (CurrentState == 3) {
+//            if (!strcmp(message, "GET_VER")) {
+//                CurrentState = 4;
+//                return MESSAGE_VALID;
+//            }
+//            else if (!strcmp(message, "GET_DATA")) {
+//                WhatCommand = 0;
+//                CurrentState = 5;
+//                return MESSAGE_VALID;
+//            }
+//            else if (!strcmp(message, "GET_FILE")) {
+//                WhatCommand = 1;
+//                CurrentState = 5;
+//                return MESSAGE_VALID;
+//            }
+//            else if (!strcmp(message, "GET_COMMAND")) {
+//                WhatCommand = 2;
+//                CurrentState = 5;
+//                return MESSAGE_VALID;
+//            }
+//            else if (!strcmp(message, "GET_B64")) {
+//                CurrentState = 6;
+//                return MESSAGE_VALID;
+//            }
+//            else if (!strcmp(message, "DISCONNECT")) {
+//                CurrentState = 7;
+//                return MESSAGE_VALID;
+//            }
+//        }
+//    }
+//    else {
+//        if (CurrentState == 2 && !strcmp(message, "CONNECT_OK")) {
+//            CurrentState = 3;
+//            return MESSAGE_VALID;
+//        }
+//        else if (CurrentState == 7 && !strcmp(message, "DISCONNECT_OK")) {
+//            CurrentState = 1;
+//            return MESSAGE_VALID;
+//        }
+//        else if (CurrentState == 4 && !strncmp(message, "VERSION ", 8)) {
+//            message += 8;
+//            if (*message > 48 && *message < 58) {
+//                for (++message; *message != '\0'; message++)
+//                    if (*message < 48 || *message > 57) {
+//                        CurrentState = 1;
+//                        return MESSAGE_INVALID;
+//                    }
+//                CurrentState = 3;
+//                return MESSAGE_VALID;
+//            }
+//        }
+//        else if (CurrentState == 5) {
+//            int l = strlen(commands[WhatCommand]);
+//            if (!strncmp(message, commands[WhatCommand], l))
+//            {
+//                message += l;
+//                if (*message == ' ')
+//                {
+//                    message++;
+//                    char* s;
+//                    for (; data[*message + 128]; ++message);
+//                    s = (*message == ' ') ? message + 1 : NULL;
+//                    if (s && !strcmp(s, commands[WhatCommand])) {
+//                        CurrentState = 3;
+//                        return MESSAGE_VALID;
+//                    }
+//                }
+//            }
+//        }
+//        else if (CurrentState == 6 && !strncmp(message, "B64: ", 5)) {
+//            message += 5;
+//            char* begin = message;
+//            for (; base64[*message + 128]; ++message);
+//            char check = 0;
+//            for (; (check < 2) && (message[check] == '='); ++check);
+//            if ((message - begin + check) % 4 == 0 && !message[check]) {
+//                CurrentState = 3;
+//                return MESSAGE_VALID;
+//            }
+//        }
+//    }
+//    CurrentState = 1;
+//    return MESSAGE_INVALID;
+//}
